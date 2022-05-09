8f08c394e75fb079cb4e696a47a418cc97c38d9e
Eugene Tsaprilov <gettosun@gmail.com>
2022-05-09 15:15:15 +0300

    feat: keyboard functions

--- keyboard/src/js/keyboard.js
+++ keyboard/src/js/keyboard.js
@@ -1,5 +1,445 @@
+class KeysEng {
+    constructor() {
+        this.Backquote = ['chars','`','~','`','~'];
+        this.Digit1 = ['chars','1','!','1','!'];
+        this.Digit2 = ['chars','2','@','2','@'];
+        this.Digit3 = ['chars','3','#','3','#'];
+        this.Digit4 = ['chars','4','$','4','$'];
+        this.Digit5 = ['chars','5','%','5','%'];
+        this.Digit6 = ['chars','6','^','6','^'];
+        this.Digit7 = ['chars','7','&','7','&'];
+        this.Digit8 = ['chars','8','*','8','*'];
+        this.Digit9 = ['chars','9','(','9','('];
+        this.Digit0 = ['chars','0',')','0',')'];
+        this.Minus = ['chars','-','_','-','_'];
+        this.Equal = ['chars','=','+','=','+'];
+        this.Backspace = ['handler','Backspace','Backspace','Backspace','Backspace'];
+        this.Tab = ['handler','Tab','Tab','Tab','Tab'];
+        this.KeyQ = ['chars','q','Q','Q','q'];
+        this.KeyW = ['chars','w','W','W','w'];
+        this.KeyE = ['chars','e','E','E','e'];
+        this.KeyR = ['chars','r','R','R','r'];
+        this.KeyT = ['chars','t','T','T','t'];
+        this.KeyY = ['chars','y','Y','Y','y'];
+        this.KeyU = ['chars','u','U','U','u'];
+        this.KeyI = ['chars','i','I','I','i'];
+        this.KeyO = ['chars','o','O','O','o'];
+        this.KeyP = ['chars','p','P','P','p'];
+        this.BracketLeft = ['chars','[','{','[','{'];
+        this.BracketRight = ['chars',']','}',']','}'];
+        this.Backslash = ['chars','\\','|','\\','|'];
+        this.Delete = ['handler','Del','Del','Del','Del'];
+        this.CapsLock = ['handler','Caps lock','Caps lock','Caps lock','Caps lock'];
+        this.KeyA = ['chars','a','A','A','a'];
+        this.KeyS = ['chars','s','S','S','s'];
+        this.KeyD = ['chars','d','D','D','d'];
+        this.KeyF = ['chars','f','F','F','f'];
+        this.KeyG = ['chars','g','G','G','g'];
+        this.KeyH = ['chars','h','H','H','h'];
+        this.KeyJ = ['chars','j','J','J','j'];
+        this.KeyK = ['chars','k','K','K','k'];
+        this.KeyL = ['chars','l','L','L','l'];
+        this.Semicolon = ['chars',';',':',';',':'];
+        this.Quote = ['chars',"'",'"',"'",'"'];
+        this.Enter = ['handler','Enter','Enter','Enter','Enter'];
+        this.ShiftLeft = ['handler','Shift','Shift','Shift','Shift'];
+        this.KeyZ = ['chars','z','Z','Z','z'];
+        this.KeyX = ['chars','x','X','X','x'];
+        this.KeyC = ['chars','c','C','C','c'];
+        this.KeyV = ['chars','v','V','V','v'];
+        this.KeyB = ['chars','b','B','B','b'];
+        this.KeyN = ['chars','n','N','N','n'];
+        this.KeyM = ['chars','m','M','M','m'];
+        this.Comma = ['chars',',','<',',','<'];
+        this.Period = ['chars','.','>','.','>'];
+        this.Slash = ['chars','/','?','/','?'];
+        this.ArrowUp = ['handler','▲','▲','▲','▲'];
+        this.ShiftRight = ['handler','Shift','Shift','Shift','Shift'];
+        this.ControlLeft = ['handler','Ctrl','Ctrl','Ctrl','Ctrl'];
+        this.MetaLeft = ['handler','Win','Win','Win','Win'];
+        this.AltLeft = ['handler','Alt','Alt','Alt','Alt'];
+        this.Space = ['handler','Space','Space','Space','Space'];
+        this.AltRight = ['handler','Alt','Alt','Alt','Alt'];
+        this.ArrowLeft = ['handler','◀','◀','◀','◀'];
+        this.ArrowDown = ['handler','▼','▼','▼','▼'];
+        this.ArrowRight = ['handler','▶','▶','▶','▶'];
+        this.ControlRight = ['handler','Ctrl','Ctrl','Ctrl','Ctrl'];
+    }
+}
+class KeysRus {
+    constructor() {
+        this.Backquote = ['chars','ё','Ё','Ё','ё'];
+        this.Digit1 = ['chars','1','!','1','!'];
+        this.Digit2 = ['chars','2','"','2','"'];
+        this.Digit3 = ['chars','3','№','3','№'];
+        this.Digit4 = ['chars','4',';','4',';'];
+        this.Digit5 = ['chars','5','%','5','%'];
+        this.Digit6 = ['chars','6',':','6',':'];
+        this.Digit7 = ['chars','7','?','7','?'];
+        this.Digit8 = ['chars','8','*','8','*'];
+        this.Digit9 = ['chars','9','(','9','('];
+        this.Digit0 = ['chars','0',')','0',')'];
+        this.Minus = ['chars','-','_','-','_'];
+        this.Equal = ['chars','=','+','=','+'];
+        this.Backspace = ['handler','Backspace','Backspace','Backspace','Backspace'];
+        this.Tab = ['handler','Tab','Tab','Tab','Tab'];
+        this.KeyQ = ['chars','й','Й','Й','й'];
+        this.KeyW = ['chars','ц','Ц','Ц','ц'];
+        this.KeyE = ['chars','у','У','У','у'];
+        this.KeyR = ['chars','к','К','К','к'];
+        this.KeyT = ['chars','е','Е','Е','е'];
+        this.KeyY = ['chars','н','Н','Н','y'];
+        this.KeyU = ['chars','г','Г','Г','г'];
+        this.KeyI = ['chars','ш','Ш','Ш','ш'];
+        this.KeyO = ['chars','щ','Щ','Щ','щ'];
+        this.KeyP = ['chars','з','З','З','з'];
+        this.BracketLeft = ['chars','х','Х','Х','х'];
+        this.BracketRight = ['chars','ъ','Ъ','Ъ','ъ'];
+        this.Backslash = ['chars','\\','/','\\','/'];
+        this.Delete = ['handler','Del','Del','Del','Del'];
+        this.CapsLock = ['handler','Caps lock','Caps lock','Caps lock','Caps lock'];
+        this.KeyA = ['chars','ф','Ф','Ф','ф'];
+        this.KeyS = ['chars','ы','Ы','Ы','ы'];
+        this.KeyD = ['chars','в','В','В','в'];
+        this.KeyF = ['chars','а','А','А','а'];
+        this.KeyG = ['chars','п','П','П','п'];
+        this.KeyH = ['chars','р','Р','Р','р'];
+        this.KeyJ = ['chars','о','О','О','о'];
+        this.KeyK = ['chars','л','Л','Л','л'];
+        this.KeyL = ['chars','д','Д','Д','д'];
+        this.Semicolon = ['chars','ж','Ж','Ж','ж'];
+        this.Quote = ['chars',"э",'Э',"Э",'э'];
+        this.Enter = ['handler','Enter','Enter','Enter','Enter'];
+        this.ShiftLeft = ['handler','Shift','Shift','Shift','Shift'];
+        this.KeyZ = ['chars','я','Я','Я','я'];
+        this.KeyX = ['chars','ч','Ч','Ч','ч'];
+        this.KeyC = ['chars','с','С','С','с'];
+        this.KeyV = ['chars','м','М','М','м'];
+        this.KeyB = ['chars','и','И','И','И'];
+        this.KeyN = ['chars','т','Т','Т','т'];
+        this.KeyM = ['chars','ь','Ь','Ь','ь'];
+        this.Comma = ['chars','б','Б','Б','б'];
+        this.Period = ['chars','ю','Ю','Ю','ю'];
+        this.Slash = ['chars','.',',','.',','];
+        this.ArrowUp = ['handler','▲','▲','▲','▲'];
+        this.ShiftRight = ['handler','Shift','Shift','Shift','Shift'];
+        this.ControlLeft = ['handler','Ctrl','Ctrl','Ctrl','Ctrl'];
+        this.MetaLeft = ['handler','Win','Win','Win','Win'];
+        this.AltLeft = ['handler','Alt','Alt','Alt','Alt'];
+        this.Space = ['handler','Space','Space','Space','Space'];
+        this.AltRight = ['handler','Alt','Alt','Alt','Alt'];
+        this.ArrowLeft = ['handler','◀','◀','◀','◀'];
+        this.ArrowDown = ['handler','▼','▼','▼','▼'];
+        this.ArrowRight = ['handler','▶','▶','▶','▶'];
+        this.ControlRight = ['handler','Ctrl','Ctrl','Ctrl','Ctrl'];
+    }
+}
+
 function keyboard() {
-    console.log('key');
+    let wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+    document.body.appendChild(wrapper);
+    let textarea = document.createElement('textarea');
+    textarea.classList.add('textarea');
+    textarea.id = 'textarea';
+    textarea.rows = '5';
+    textarea.cols = '50';
+    textarea.autofocus = true;
+    wrapper.appendChild(textarea);
+    let keyboard = document.createElement('div');
+    keyboard.classList.add('keyboard');
+    wrapper.appendChild(keyboard);
+    textarea.value = '';
+    let caretPosition = 0;
+    let langSwitchCtrl = 0;
+    let langSwitchAlt = 0;
+    const keyEng = new KeysEng();
+    const keyEngNames = Object.keys(keyEng);
+    const keyEngValues = Object.values(keyEng);
+    const keyRus = new KeysRus();
+    const keyRusNames = Object.keys(keyRus);
+    const keyRusValues = Object.values(keyRus);
+    let n = keyEngNames;
+    let v = keyEngValues;
+    let caps = 0;
+
+    start(n, v, 1);
+
+    function start(names, values, n) {
+        for(let i = 0; i < values.length; i++) {
+            const key = `<div class="key ${names[i]} ${values[i][0]}">${values[i][n]}</div>`;
+            keyboard.insertAdjacentHTML('beforeend', key);
+        }
+    }
+
+    keyboard.addEventListener('click', function(event) {
+        if(event.target.matches('.chars')) {
+            textarea.value = textarea.value.substring(0, caretPosition) + event.target.innerHTML + textarea.value.substring(caretPosition);
+            caretPosition++;
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(event.target.matches('.Backspace')) {
+            --caretPosition;
+            textarea.value = textarea.value.slice(0, caretPosition) + textarea.value.slice(caretPosition + 1);
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(event.target.matches('.Delete')) {
+            textarea.value = textarea.value.slice(0, caretPosition) + textarea.value.slice(caretPosition + 1);
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(event.target.matches('.Tab')) {
+            textarea.value = textarea.value.substring(0, caretPosition) + '    ' + textarea.value.substring(caretPosition);
+            caretPosition += 4;
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(event.target.matches('.CapsLock')) {
+            keyboard.innerHTML = '';
+            caps++;
+            if(caps % 2 === 0) {
+                start(n, v, 1);
+            } else {
+                start(n, v, 3);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(event.target.matches('.Enter')) {
+            textarea.value = textarea.value.substring(0, caretPosition) + '\n' + textarea.value.substring(caretPosition);
+            caretPosition++;
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(event.target.matches('.Space')) {
+            textarea.value = textarea.value.substring(0, caretPosition) + ' ' + textarea.value.substring(caretPosition);
+            caretPosition++;
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        
+        if(event.target.matches('.ArrowLeft')) {
+            caretPosition--;
+            if(caretPosition < 0) {
+                caretPosition = 0;
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+            console.log(caretPosition);
+        }
+        if(event.target.matches('.ArrowRight')) {
+            caretPosition++;
+            if(caretPosition > textarea.value.length) {
+                caretPosition = textarea.value.length;
+            }
+            textarea.focus();
+            textarea.selectionStart = caretPosition;
+            console.log(caretPosition);
+        }
+        //console.log(textarea.selectionEnd);
+    });
+
+    keyboard.addEventListener('mousedown', function(event) {
+        if(event.target.matches('.ShiftLeft')) {
+            keyboard.innerHTML = '';
+            if(caps % 2 === 0) {
+                start(keyEngNames, keyEngValues, 2);
+            } else {
+                start(keyEngNames, keyEngValues, 4);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(event.target.matches('.ShiftRight')) {
+            keyboard.innerHTML = '';
+            if(caps % 2 === 0) {
+                start(keyEngNames, keyEngValues, 2);
+            } else {
+                start(keyEngNames, keyEngValues, 4);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+    });
+    keyboard.addEventListener('mouseup', function(event) {
+        if(event.target.matches('.ShiftLeft')) {
+            keyboard.innerHTML = '';
+            if(caps % 2 === 0) {
+                start(keyEngNames, keyEngValues, 1);
+            } else {
+                start(keyEngNames, keyEngValues, 3);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(event.target.matches('.ShiftRight')) {
+            keyboard.innerHTML = '';
+            if(caps % 2 === 0) {
+                start(keyEngNames, keyEngValues, 1);
+            } else {
+                start(keyEngNames, keyEngValues, 3);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+    });
+
+    textarea.addEventListener('keyup', e => {
+        caretPosition = e.target.selectionStart;
+    });
+    textarea.addEventListener('click', e => {
+        caretPosition = e.target.selectionStart;
+    });
+
+    document.onkeydown = function(e) {
+        const inputKey = '.' + e.code;
+        if(e.code == 'Tab') {
+            textarea.value = textarea.value.substring(0, caretPosition) + '    ' + textarea.value.substring(caretPosition);
+            caretPosition += 4;
+            keyAnim();
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+            return e.preventDefault();
+        }
+        if(e.code == 'CapsLock') {
+            keyboard.innerHTML = '';
+            caps++;
+            if(caps % 2 === 0) {
+                start(n, v, 1);
+            } else {
+                start(n, v, 3);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+            return e.preventDefault();
+        }
+        if(e.code == 'ShiftLeft') {
+            keyboard.innerHTML = '';
+            if(caps % 2 === 0) {
+                start(n, v, 2);
+            } else {
+                start(n, v, 4);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(e.code == 'ShiftRight') {
+            keyboard.innerHTML = '';
+            if(caps % 2 === 0) {
+                start(n, v, 2);
+            } else {
+                start(n, v, 4);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(e.code == 'ControlLeft') {
+            langSwitchCtrl++;
+            if(langSwitchAlt > 0) {
+                changeLang();
+            }
+            keyAnim();
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+            return e.preventDefault();
+        }
+        if(e.code == 'ControlRight') {
+            keyAnim();
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+            return e.preventDefault();
+        }
+        if(e.code == 'AltLeft') {
+            langSwitchAlt++;
+            if(langSwitchCtrl > 0) {
+                changeLang();
+            }
+            keyAnim();
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+            return e.preventDefault();
+        }
+        if(e.code == 'AltRight') {
+            keyAnim();
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+            return e.preventDefault();
+        }
+         else {
+            keyAnim();
+        }
+
+        function keyAnim() {
+            document.querySelector(inputKey).classList.add('active');
+            //document.querySelector(inputKey).dispatchEvent(new Event("click"));
+            document.querySelector(inputKey).click();
+            return e.preventDefault();
+        }
+    };
+    document.onkeyup = function(e) {
+        const inputKey = '.' + e.code;
+
+        if(e.code == 'ShiftLeft') {
+            keyboard.innerHTML = '';
+            if(caps % 2 === 0) {
+                start(n, v, 1);
+            } else {
+                start(n, v, 3);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(e.code == 'ShiftRight') {
+            keyboard.innerHTML = '';
+            if(caps % 2 === 0) {
+                start(n, v, 1);
+            } else {
+                start(n, v, 3);
+            }
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(e.code == 'ControlLeft') {
+            langSwitchCtrl = 0;
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+        if(e.code == 'AltLeft') {
+            langSwitchAlt = 0;
+            textarea.focus();
+            textarea.selectionEnd = caretPosition;
+        }
+
+        keyDisp();
+        function keyDisp() {
+            const dispatchAnim = setTimeout(() => {
+                document.querySelector(inputKey).classList.remove('active');
+            }, 100);
+            dispatchAnim;
+        }
+    };
+    function changeLang() {
+        let capsSwitcher = 0;
+        if(caps % 2 === 0) {
+            capsSwitcher = 1;
+        } else {
+            capsSwitcher = 3;
+        }
+        if(document.querySelector('.KeyQ').innerHTML == 'q' || document.querySelector('.KeyQ').innerHTML == 'Q') {
+            n = keyRusNames;
+            v = keyRusValues;
+            keyboard.innerHTML = '';
+            start(keyRusNames, keyRusValues, capsSwitcher);
+        } else {
+            n = keyEngNames;
+            v = keyEngValues;
+            keyboard.innerHTML = '';
+            start(keyEngNames, keyEngValues, capsSwitcher);
+        }
+    }
+    
 }
 
 export default keyboard;
