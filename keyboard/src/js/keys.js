9ce0e7a79d9fcb6350731702ec2823bc92bab2ff
Eugene Tsaprilov <gettosun@gmail.com>
2022-05-09 15:16:30 +0300

    feat: keyboard funcs

---
+++ keyboard/src/js/keys.js
@@ -0,0 +1,27 @@
+class KeysEng {
+    constructor() {
+        this.Backquote = ['`','~','`','~'];
+        this.Digit1 = ['1','!','1','!'];
+        this.Digit2 = ['2','@','2','@'];
+        this.Digit3 = ['3','#','3','#'];
+        this.Digit4 = ['4','$','4','$'];
+        this.Digit5 = ['5','%','5','%'];
+        this.Digit6 = ['6','^','6','^'];
+        this.Digit7 = ['7','&','7','&'];
+        this.Digit8 = ['8','*','8','*'];
+        this.Digit9 = ['9','(','9','('];
+        this.Digit0 = ['0',')','0',')'];
+        this.Minus = ['-','_','-','_'];
+        this.Equal = ['=','+','=','+'];
+        this.Backspace = ['Backspace','Backspace','Backspace','Backspace'];
+    }
+}
+
+const justTo = new KeysEng();
+console.log(justTo);
+/* const keysEng = new KeysEng();
+const keyNames = Object.keys(keysEng);
+const keyValues = Object.values(keysEng);
+
+console.log(keyNames);
+console.log(keyValues); */
